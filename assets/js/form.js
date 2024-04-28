1be3a2730237e1b1f5b8065bc01fc20939e910b0
BreeNous <breeanna.nouskhajian@gmail.com>
2024-04-28 14:38:14 -0400

    added new styles to html elements so that when the theme button is clicked the function will execute to change the current styles to the dark mode styles and vise versa.

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,11 +1,10 @@
-// I don't understand why this code wont work, it keeps givimg me "Uncaught TypeError"
-// I also what to understand why we need 500 different files for this project
-
+// Grabbing elements from html
 const usernameInput = document.getElementById("username");
 const titleInput = document.getElementById("title");
 const contentInput = document.getElementById("content");
 const submitButton = document.getElementById("submit");
 
+// saves user input to local storage and clears the input fields for next entry upon submit button click.
 let saveToLocalStorage = function() {
     let blogPostData = {
         userName: usernameInput.value,
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,67 @@
-// const moon = document.getElementsByClassName('sun');
+let themeButton = document.getElementById("theme")
+let changeButton = themeButton
+let body = document.getElementById("body");
+let header = document.getElementById("header");
+let middle = document.getElementById("middle");
+let statement = document.getElementById("statement");
+let userName = document.getElementById("userName");
+let blogTitle = document.getElementById("blogTitle");
+let blogContent = document.getElementById("blogContent");
+let submit = document.getElementById("submit");
+let username = document.getElementById("username");
+let title = document.getElementById("title");
+let content = document.getElementById("content");
 
-// toggleTheme.addEventListener('click', function (event) {
+let isNightMode = false;
 
-    
+let changeTheme = function () {
 
-// })
+    if (!isNightMode) {
+        changeButton.textContent = "🌙"
+        body.style.backgroundColor = "black";
+        header.style.borderColor = "white";
+        middle.style.borderColor = "white";
+        statement.style.color = "white";
+        userName.style.color = "white";
+        blogTitle.style.color = "white";
+        blogContent.style.color = "white";
+        submit.style.color = "black";
+        submit.style.backgroundColor = "white";
+        username.style.borderColor = "white";
+        username.style.backgroundColor = "black";
+        username.style.color = "white";
+        title.style.borderColor = "white";
+        title.style.backgroundColor = "black";
+        title.style.color = "white";
+        content.style.borderColor = "white";
+        content.style.backgroundColor = "black";
+        content.style.color = "white";
+
+        isNightMode = true;
+    } else {
+        changeButton.textContent = "☀️"
+        body.style.backgroundColor = "white";
+        header.style.borderColor = "black";
+        middle.style.borderColor = "black";
+        statement.style.color = "black";
+        userName.style.color = "black";
+        blogTitle.style.color = "black";
+        blogContent.style.color = "black";
+        submit.style.color = "white";
+        submit.style.backgroundColor = "black";
+        username.style.borderColor = "black";
+        username.style.backgroundColor = "white";
+        username.style.color = "black";
+        title.style.borderColor = "black";
+        title.style.backgroundColor = "white";
+        title.style.color = "black";
+        content.style.borderColor = "black";
+        content.style.backgroundColor = "white";
+        content.style.color = "black";
+
+
+        isNightMode = false;
+    }
+}
+
+themeButton.addEventListener('click', changeTheme);
